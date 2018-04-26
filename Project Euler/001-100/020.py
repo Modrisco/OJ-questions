ca9fba78858153cfca1ca23d627d35cc87357e08
rockiemlx <menglingxu95@gmail.com>
2018-04-26 19:28:17 +1000

    question

--- Project Euler/001-100/020.py
+++ Project Euler/001-100/020.py
@@ -1,7 +1,2 @@
-def f(n):
-    if n == 1:
-        return 1
-    if n > 1:
-        return n * f(n - 1)
-
-print(sum(map(int, str(f(100)))))    
+from math import factorial
+print(sum(map(int, str(factorial(100)))))    
