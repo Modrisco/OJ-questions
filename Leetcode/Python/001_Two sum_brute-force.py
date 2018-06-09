a20bc7811f917ae511fa5e905b5b8e148888bc8e
rockiemlx <menglingxu95@gmail.com>
2018-06-09 22:07:34 +1000

    arrays

---
+++ Leetcode/Python/001_Two sum_brute-force.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        answer = []
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    answer.append(i)
+                    answer.append(j)
+        return answer
+        
