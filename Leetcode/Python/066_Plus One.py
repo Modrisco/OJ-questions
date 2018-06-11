490abb6d9cb7bd09fe4a842dca8bfdd76b48bbbc
Joseph.L.Meng <menglingxu95@gmail.com>
2018-06-11 22:59:38 +1000

    Update 066_Plus One.py

--- Leetcode/Python/066_Plus One.py
+++ Leetcode/Python/066_Plus One.py
@@ -4,5 +4,6 @@ class Solution:
         :type digits: List[int]
         :rtype: List[int]
         """
+        # Convert string into an integer, plus one. Finally, convert the integer back to string and return a list
         return list(map(int, str(int(''.join(map(str, digits))) + 1)))
         
