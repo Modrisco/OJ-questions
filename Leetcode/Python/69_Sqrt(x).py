b966033ee06da0ba511128ac376c35eea1db5680
JosephMLX <menglingxu95@gmail.com>
2019-02-16 03:00:44 +0800

    2.15

---
+++ Leetcode/Python/69_Sqrt(x).py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        from math import sqrt
+        return int(sqrt(x))
+
+    # return int(x ** 0.5)
